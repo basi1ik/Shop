d802807eaf646279cee528bcd74949215dd9b307
Basilik <basilikmitrofun@gmail.com>
2021-02-02 23:17:05 +0800

    Add project files.

---
+++ Shop/Controllers/CartController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: CartController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Shop/Models/SeedData.cs
@@ -0,0 +1,52 @@
+﻿using Shop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shop.Models
+{
+    public static class SeedData
+    {
+        public static  void Initailize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ShopContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ShopContext>>()))
+            {
+                if (context.Product.Any())
+                {
+                    return;
+                }
+
+                context.Product.AddRange(
+                  
+                    new Product                     
+                    { 
+                        Name = "Apple",
+                        Price = 1000M,
+                        Balance = 400
+                    },
+
+                    new Product
+                    {
+                        Name = "Milk",
+                        Price = 1000M,
+                        Balance = 300
+                    },
+
+                    new Product
+                    {
+                        Name = "Bread",
+                        Price = 2000M,
+                        Balance = 330
+                    }
+                );
+
+                context.SaveChanges();
+            }     
+        }
+    }
+}
